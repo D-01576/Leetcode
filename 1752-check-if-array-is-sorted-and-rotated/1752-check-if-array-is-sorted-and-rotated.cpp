cf676800aa1a996a1e41f3b9246b200fcd14568e
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-08-27 18:09:01 +0500

    Time: 0 ms (100.00%), Space: 10.5 MB (49.78%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -8,10 +8,9 @@ public:
         for (int i = 0; i < nums.size(); ++i) {
             if (nums[i] > nums[(i + 1) % nums.size()]) {
                 count++;
-                if (count > 1) return false;
             }
         }
         
-        return true;
+        return count <= 1;
     }
 };
