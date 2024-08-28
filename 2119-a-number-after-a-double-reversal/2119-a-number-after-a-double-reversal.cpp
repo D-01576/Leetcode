841bc8ab814b8b58a12d87963386adc7455f0114
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-08-28 17:24:33 +0500

    Time: 3 ms (26.78%), Space: 7.5 MB (7.50%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        int x = num;
-        int ans = 0;
-        int again = 0;
-        while(num > 0){
-            ans = ans * 10 + (num % 10);
-            num /= 10;
+        // int x = num;
+        // int ans = 0;
+        // int again = 0;
+        // while(num > 0){
+        //     ans = ans * 10 + (num % 10);
+        //     num /= 10;
+        // }
+        // while(ans > 0){
+        //     again = again * 10 + (ans % 10);
+        //     ans /= 10;
+        // }
+        // return x == again; 
+        string j = to_string(num);
+        if(j.size() < 2){
+            return true;
         }
-        while(ans > 0){
-            again = again * 10 + (ans % 10);
-            ans /= 10;
-        }
-        return x == again; 
+        return j[j.size()-1] != '0';
     }
 };
