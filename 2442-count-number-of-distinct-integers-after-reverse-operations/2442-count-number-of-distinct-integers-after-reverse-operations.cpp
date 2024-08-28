5c8d2a599b2d5cf2ded957b38c191e24a3f21753
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-08-28 18:05:09 +0500

    Time: 350 ms (25.60%), Space: 112.4 MB (41.68%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,22 +1,20 @@
+#include <vector>
+#include <set>
+using namespace std;
+
 class Solution {
 public:
     int countDistinctIntegers(vector<int>& nums) {
-        vector<int> neww;
-        vector<int> newww;
-        
-        for(int i = 0 ; i < nums.size();i++){
-            int reversedNum = 0;
-            int temp = nums[i];
+        set<int> distinctElements;
+        for (int num : nums) {
+            distinctElements.insert(num); 
+            int reversedNum = 0, temp = num;
             while (temp > 0) {
                 reversedNum = reversedNum * 10 + temp % 10;
                 temp /= 10;
             }
-            neww.push_back(reversedNum);
-            newww.push_back(reversedNum);
-            newww.push_back(nums[i]);
+            distinctElements.insert(reversedNum);
         }
-        set<int> distinctElements(newww.begin(), newww.end());
-        nums = neww;
         return distinctElements.size();
     }
 };
