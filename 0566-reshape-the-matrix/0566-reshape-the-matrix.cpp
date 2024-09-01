fd77767caebeea563b6b8a186c54a6dde8d25472
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-09-01 13:04:12 +0500

    Time: 12 ms (18.39%), Space: 14 MB (9.58%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int oldRows = mat.size();
-        int oldCols = mat[0].size();
-        if (oldRows * oldCols != r * c) {
+       vector<vector<int>> ans(r, vector<int>(c));  
+        int now = 0;
+        
+        if (mat.size()*mat[0].size() != r * c) {
             return mat; 
         }
-        vector<vector<int>> ans(r, vector<int>(c));
-        for (int i = 0; i < oldRows; i++) {
-            for (int j = 0; j < oldCols; j++) {
-                int flatIndex = i * oldCols + j;
-                int newRow = flatIndex / c;
-                int newCol = flatIndex % c;
-                ans[newRow][newCol] = mat[i][j];
+        int tempr = 0;
+        int tempc = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[i].size(); j++) {
+                ans[tempr][tempc] = mat[i][j];
+                tempc++;
+                if(tempc == c){
+                    tempr++;
+                    tempc = 0;
+                }
             }
         }
+        
         return ans;
     }
 };
