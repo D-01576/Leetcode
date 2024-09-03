ab9096d58e96d6e6b2009a1aaf5afd9c6ed0098e
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-09-03 14:29:53 +0500

    Time: 3 ms (26.51%), Space: 9.6 MB (12.39%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        string numString = to_string(num);
+        
+        while(numString.size() != 1){
+            int temp = 0;
+            for(int i = 0 ; i < numString.size();i++){
+                temp += numString[i]-'0';
+            }
+            
+            numString = to_string(temp);
+        }
+        return stoi(numString);
+    }
+};
