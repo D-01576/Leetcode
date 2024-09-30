eb4ab6216f17a99382ac23f714bc7c768fa5629f
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-09-30 14:05:54 +0500

    Time: 27 ms (65.70%), Space: 26 MB (66.84%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -1,45 +1,33 @@
 class CustomStack {
 public:
-    CustomStack(int maxSize) {
-        size = maxSize;
-    }
-    
-    void push(int x) {
-        if(current < size){
-            stack.push_back(x);
-            current++;
-        }
-    }
-    
-    int pop() {
-        if(current == 0){
-            return -1;
-        }
-        int ret = stack[stack.size()-1];
-        stack.pop_back();
-        current--;
-        return ret;
-    }
-    
-    void increment(int k, int val) {
-        if(k > current){
-            k = current;
-        }
-        
-        for(int i = 0 ; i < k ; i++){
-            stack[i] += val;
-        }
-    }
-private:
-    int current = 0;
-    int size = 0;
-    vector<int> stack;
-};
+    int n;
+    stack<int> stack;
+    vector<int> inc;
 
-/**
- * Your CustomStack object will be instantiated and called as such:
- * CustomStack* obj = new CustomStack(maxSize);
- * obj->push(x);
- * int param_2 = obj->pop();
- * obj->increment(k,val);
- */
+    CustomStack(int n) {
+        this->n = n;
+    }
+
+    void push(int x) {
+        if (stack.size() < n) {
+            stack.push(x);
+            inc.push_back(0);
+        }
+    }
+
+    int pop() {
+        if (stack.empty()) return -1;
+        if (inc.size() > 1) inc[inc.size() - 2] += inc.back();
+        int res = stack.top() + inc.back();
+        stack.pop();
+        inc.pop_back();
+        return res;
+    }
+
+    void increment(int k, int val) {
+        if (!stack.empty()) {
+            int idx = min(k, (int)inc.size()) - 1;
+            inc[idx] += val;
+        }
+    }
+};
