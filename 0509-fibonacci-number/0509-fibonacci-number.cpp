c300ec2444462429a2d9858d41e8c81346d41c60
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-10-01 22:27:36 +0500

    Time: 2 ms (80.99%), Space: 7.3 MB (78.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,6 +2,13 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0 || n == 1) return n;
-        return fib(n-1)+fib(n-2); 
+        int prev1 = 0;
+        int prev2 = 1;
+        for(int i = 0 ; i < n-1;i++){
+            int th = prev1+prev2;
+            prev1 = prev2;
+            prev2 = th;
+        }
+        return prev2;
     }
 };
