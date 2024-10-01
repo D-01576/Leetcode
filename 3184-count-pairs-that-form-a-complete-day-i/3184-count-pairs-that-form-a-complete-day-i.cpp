8a2874c83a38aa617e6cdba9106daef9525e75d2
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-10-01 15:54:00 +0500

    Time: 0 ms (100.00%), Space: 23.2 MB (81.00%) - LeetHub

--- 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.cpp
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int countCompleteDayPairs(vector<int>& hours) {
-        int ans = 0;
-        for(int i = 0 ; i < hours.size();i++){
-            for(int j = i + 1;j < hours.size();j++){
-                if((hours[i]+hours[j]) % 24 == 0){
-                    ans++;
-                }
-            }
-        }
-        return ans;
+        // int ans = 0;
+        // for(int i = 0 ; i < hours.size();i++){
+        //     for(int j = i + 1;j < hours.size();j++){
+        //         if((hours[i]+hours[j]) % 24 == 0){
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
+         const int DAY = 24;
+        for (auto &hour: hours) hour = hour % DAY;
+
+        int res = 0;
+        vector<int> count(24, 0);
+        for (auto hour: hours) 
+            res += count[(DAY - hour) % DAY],
+            count[hour]++;
+        return res;
     }
 };
