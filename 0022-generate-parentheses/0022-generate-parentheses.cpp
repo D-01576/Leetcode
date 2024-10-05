a7274afc42feae08422caa17c04cdffe2a83d069
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-10-05 17:53:59 +0500

    Time: 0 ms (100.00%), Space: 9.1 MB (99.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,28 @@
 class Solution {
 private:
     vector<string> ans;
+
 public:
-    void th(int n, int open, int close, string k){
-        if(k.size() == n*2){
+    void th(int n, int open, int close, string& k, int idx) {
+        if (idx == n * 2) {
             ans.push_back(k);
             return;
         }
-        
-        if(close < open){
-            th(n,open,close+1,k+')');
+
+        if (open < n) {
+            k[idx] = '(';
+            th(n, open + 1, close, k, idx + 1);
         }
-        
-        if(open < n){
-            th(n,open+1,close,k+'(');
+
+        if (close < open) {
+            k[idx] = ')';
+            th(n, open, close + 1, k, idx + 1);
         }
     }
+
     vector<string> generateParenthesis(int n) {
-        th(n,0,0,"");
+        string k(2 * n, ' ');
+        th(n, 0, 0, k, 0);
         return ans;
     }
 };
