ab23e0d2bb7cdcb8a8dd361e9c2eb03a2ab39950
Siraj Khatti <137029391+Siraj01576@users.noreply.github.com>
2024-10-10 22:29:32 +0500

    Time: 32 ms (8.90%), Space: 53.6 MB (5.05%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,49 @@
+#define pb push_back
+
+class Solution {
+private:
+    vector<vector<string>> ans;
+public:
+    bool isatack(int x,int col,vector<string> s){
+        int r =x;
+        int c = col;
+        while ( x >= 0 && col >= 0 ){       
+            if(s[x][col]=='Q')  return true;
+            x--;
+            col--;      
+        }
+        x= r;
+        col=c;
+        while ( x <  s.size() && col >= 0 ){
+            if(s[x][col]=='Q') return true;
+            x++;
+            col--;
+        }
+        x= r;
+        col=c; 
+        while( col >= 0 ){
+            if (s[x][col]=='Q') return true;
+            col--;
+        }  
+        return false;
+    }
+    void solve(int col,vector<string> s, int n){
+        if(col == n){
+            ans.pb(s);
+            return;
+        }
+        
+        for(int i = 0 ; i < n;i++){
+            if(!isatack(i,col,s)){
+                s[i][col] = 'Q';
+                solve(col+1,s,n);
+                s[i][col] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string> s ( n , string (n,'.') );
+        solve(0,s,n);
+        return ans.size();
+    }
+};
