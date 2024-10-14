dec6687e1b0247d6eaf73f57436074e3b6f62a19
Waqar Ahmed <137029391+D-01576@users.noreply.github.com>
2024-10-14 17:35:31 +0500

    Time: 145 ms (97.26%), Space: 76 MB (79.55%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -5,12 +5,14 @@ public:
         long long ans = 0;
         for (int i = 0; i < k; i++) {
             int num = n.top(); 
-            n.pop(); 
-            
             ans += num;
-            
+            if(num == 1){
+                ans+=(k-1-i);
+                break;
+            }
             num = ceil(num / 3.0);
             
+            n.pop(); 
             n.push(num);
         }
 
