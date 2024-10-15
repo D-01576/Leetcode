ed3fa20f964e6283b07db0f4e3ecf0344831cb2d
Waqar Ahmed <137029391+D-01576@users.noreply.github.com>
2024-10-15 22:09:08 +0500

    Time: 128 ms (25.02%), Space: 80.3 MB (70.45%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        int ans = 0;
-        sort(players.begin(),players.end());
+        sort(players.begin(), players.end());
         sort(trainers.begin(), trainers.end());
-        int i = 0, j =0;
-        while(i < players.size() && j < trainers.size()){
-            if(players[i] <= trainers[j]){
+        
+        int i = 0, j = 0, ans = 0;
+        
+        while (i < players.size() && j < trainers.size()) {
+            if (players[i] <= trainers[j]) {
                 ans++;
-                j++;
                 i++;
-            }else {
-                j++;
             }
+            j++;
         }
+        
         return ans;
     }
 };
