23018e71f75d90f45542d72cd0279764440b369f
Waqar Ahmed <137029391+D-01576@users.noreply.github.com>
2024-10-17 17:51:27 +0500

    Time: 77 ms (54.21%), Space: 49.9 MB (66.84%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.cpp
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.cpp
@@ -3,25 +3,31 @@ public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int> ans;
         int tAdd = 0;
-        for(int n : nums){
-            if(n % 2 == 0){
+
+        for (int n : nums) {
+            if ((n & 1) == 0) {
                 tAdd += n;
             }
         }
-        for(const auto& qu : queries){
-            int before = nums[qu[1]];
-            int sum = before + qu[0];
-            nums[qu[1]] = sum;
-            int after = nums[qu[1]];
-            if(before % 2 == 0){
+
+        for (const auto& qu : queries) {
+            int index = qu[1];
+            int valueToAdd = qu[0];
+            int before = nums[index];
+
+            nums[index] += valueToAdd;
+            int after = nums[index];
+
+            if ((before & 1) == 0) {
                 tAdd -= before;
             }
-            if(after % 2 == 0){
+            if ((after & 1) == 0) {
                 tAdd += after;
             }
-            
+
             ans.push_back(tAdd);
         }
+
         return ans;
     }
 };
