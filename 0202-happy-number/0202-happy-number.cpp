d3aecada12c3adec0dd07255d068ebf6dcfdd17f
Sarfaraz ahmed <137029391+D-01576@users.noreply.github.com>
2024-11-13 19:05:59 +0500

    Time: 0 ms (100.00%), Space: 9.3 MB (5.71%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        bool ishap = false;
+        int times = 0;
+        string num = to_string(n);
+        while(!ishap && times < 20) {
+            int number = 0;
+            for(int i = 0; i < num.size(); i++){
+                number += (num[i] - '0') * (num[i] - '0');
+            }
+            if(number == 1){
+                ishap = true;
+            } else {
+                num = to_string(number);
+            }
+            times++;
+        }
+        return ishap;
+    }
+};
