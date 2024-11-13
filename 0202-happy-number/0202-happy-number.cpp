6826a6ed7f5d8f852293fb4a91963bd8e3f120f9
Sarfaraz ahmed <137029391+D-01576@users.noreply.github.com>
2024-11-13 19:06:25 +0500

    Time: 0 ms (100.00%), Space: 9 MB (8.55%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -4,7 +4,7 @@ public:
         bool ishap = false;
         int times = 0;
         string num = to_string(n);
-        while(!ishap && times < 15) {
+        while(!ishap && times < 10) {
             int number = 0;
             for(int i = 0; i < num.size(); i++){
                 number += (num[i] - '0') * (num[i] - '0');
