70204474b7e363ee130e378b694cb40dd092705d
Sarfaraz ahmed <137029391+D-01576@users.noreply.github.com>
2024-12-03 18:32:08 +0500

    Time: 33 ms (9.04%), Space: 85.7 MB (15.24%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -1,15 +1,39 @@
+// class Solution {
+// public:
+//     string addSpaces(string s, vector<int>& spaces) {
+//         string ans = "";
+//         sort(spaces.begin(),spaces.end());
+//         int current = 0;
+//         for(int i = 0; i < spaces.size();i++){
+//             ans += s.substr(current, spaces[i]-current);
+//             current = spaces[i];
+//             ans += " ";
+//         }
+//         ans += s.substr(current, s.size()-current);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
         string ans = "";
         sort(spaces.begin(),spaces.end());
         int current = 0;
-        for(int i = 0; i < spaces.size();i++){
-            ans += s.substr(current, spaces[i]-current);
-            current = spaces[i];
-            ans += " ";
+        bool spaceavail = true;
+        if(spaces.size()-1 < current){
+            spaceavail = false;
         }
-        ans += s.substr(current, s.size()-current);
+        for(int i = 0 ; i < s.size();i++){
+            if(spaceavail && spaces[current] == i){
+                ans += " ";
+                current++;
+                if(spaces.size()-1 < current){
+                    spaceavail = false;
+                }
+            }
+            ans += s[i];
+        }
         return ans;
     }
 };
