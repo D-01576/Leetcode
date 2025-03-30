0069154f48facfc8df4ed656b251db25833b5778
Waqar Ahmed <137029391+D-01576@users.noreply.github.com>
2025-03-30 14:42:06 +0500

    Time: 0 ms (100%), Space: 26.4 MB (99.89%) - LeetHub

--- 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
+++ 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> minCosts(vector<int>& cost) {
-        vector<int> ans;
-        int current = cost[0];
-        ans.push_back(current);
-        for(int i = 1; i < cost.size();i++){
-            if(current > cost[i]){
-                current = cost[i];
-            }
-            ans.push_back(current);
+        int n = cost.size();
+        vector<int> answer(n);
+        int min_cost = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            min_cost = min(min_cost, cost[i]);
+            answer[i] = min_cost;
         }
-        return ans;
+        return answer;
     }
 };
