e440a3bb86032ec7a1203b86afe1952145167101
Waqar Ahmed <137029391+D-01576@users.noreply.github.com>
2025-04-08 21:58:26 +0500

    Time: 5 ms (37.88%), Space: 27.8 MB (43.34%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -4,10 +4,10 @@ public:
         unordered_map<int, int> check;
 
         for(int i = nums.size()-1; i >=0 ;i--){
-            check[nums[i]]++;
-            if(check[nums[i]] > 1){
+            if(check.find(nums[i]) != check.end()){
                 return i/3+1;
             }
+            check[nums[i]]++;
         }
         return 0;
     }
