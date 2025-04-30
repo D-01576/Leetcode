746c3069810d3362a123ab984bd4700f830505dc
Waqar Ahmed <137029391+D-01576@users.noreply.github.com>
2025-05-01 00:05:59 +0500

    Time: 0 ms (100%), Space: 8.5 MB (62.52%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        if(word1.size() == 0) return word2;
+        if(word2.size() == 0) return word1;
         string ans = "";
         int one = word1.size(), two = word2.size();
         for(int i = 0; i < max(one,two);i++){
